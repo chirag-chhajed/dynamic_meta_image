388762d5df5d2110dcf479e6f62d735f926d6d8d
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-03-25 14:26:19 +0530

    stash

---
+++ src/components/Meta.jsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const Meta = ({ title, description, imageUrl }) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={imageUrl} />
+
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={imageUrl} />
+    </Head>
+  );
+};
+
+export default Meta;
