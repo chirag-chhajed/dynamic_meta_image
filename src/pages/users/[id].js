b3ba4b71c0f8bd46f1e8eef586d39c63d8585a10
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-03-26 14:41:12 +0530

    change made

--- src/pages/users/[id].js
+++ src/pages/users/[id].js
@@ -17,7 +17,31 @@ const Id = ({ data, imageDataUrl }) => {
   return (
     <>
       <Head>
+        {/* <title></title>
+        <meta property="og:title" content={data.name} />
+        <meta
+          property="og:description"
+          content=
+        />
+        <meta property="og:image" content={imageDataUrl} />
+
+        <meta property="twitter:title" content={data.name} />
+        <meta property="twitter:description" content={data.name} />
+        <meta property="twitter:image" content={imageDataUrl} />
+        <!-- Primary Meta Tags --> */}
         <title>{data.name}</title>
+        <meta name="title" content={data.name} />
+        <meta
+          name="description"
+          content={data.name + data.email + data.website}
+        />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://dynamic-meta-image.vercel.app/"
+        />
         <meta property="og:title" content={data.name} />
         <meta
           property="og:description"
@@ -25,13 +49,22 @@ const Id = ({ data, imageDataUrl }) => {
         />
         <meta property="og:image" content={imageDataUrl} />
 
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://dynamic-meta-image.vercel.app"
+        />
         <meta property="twitter:title" content={data.name} />
-        <meta property="twitter:description" content={data.name} />
+        <meta
+          property="twitter:description"
+          content={data.name + data.email + data.website}
+        />
         <meta property="twitter:image" content={imageDataUrl} />
       </Head>
       <section className="p-4">
         <Card {...data} />
       </section>
+      <img src={imageDataUrl} alt={data.name} />
     </>
   );
 };
